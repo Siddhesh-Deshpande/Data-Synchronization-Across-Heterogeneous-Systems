8b7737bc80c3ae33b6e94f19fdf13eb9b31fa04d
Siddhesh Deshpande <siddhesh.deshpande@iiitb.ac.in>
2025-04-19 10:45:33 +0000

    PostgreSQL and MongoDB done

---
+++ src/main/java/com/nosql/Hive.java
@@ -0,0 +1,5 @@
+package com.nosql;
+import com.nosql.server;
+public class Hive implements server{
+    
+}
--- src/main/java/com/nosql/Main.java
+++ src/main/java/com/nosql/Main.java
@@ -1,7 +1,34 @@
 package com.nosql;
-
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            PostgreSQL psql = new PostgreSQL();
+            psql.set("SID1033", "CSE016","B");
+            ArrayList<String>res = psql.get("SID1033", "CSE016");
+            MongoDB mdb = new MongoDB();
+            mdb.set("SID1033", "CSE016","C");
+            ArrayList<String>res1 = mdb.get("SID1033", "CSE016");
+            // System.out.println(res1.size());
+            System.out.println("PostgreSQL:");
+            for(String it:res)
+            {
+                System.out.print(it+" ");
+            }
+            System.out.println();
+            System.out.println("MongoDB:");
+            for(String it:res1)
+            {
+                System.out.print(it+" ");
+            }
+            System.out.println();
+            psql.merge("mongo");
+            
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
